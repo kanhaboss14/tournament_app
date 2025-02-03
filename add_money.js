613c1137fde25b73591e7fcc324d9823adc335c7
kanhaboss14 <kanhaboss1432@gmail.com>
2025-02-03 16:25:54 +0530

    Update add_money.js

--- add_money.js
+++ add_money.js
@@ -12,7 +12,7 @@ function showQR() {
         return;
     }
 
-    const upiId = 'createprincemahto-2@okaxis';
+    const upiId = '9079078718@pthdfc';
     const upiLink = `upi://pay?pa=${upiId}&am=${amount}&cu=INR`;
 
     const qr = qrcode(0, 'M');
